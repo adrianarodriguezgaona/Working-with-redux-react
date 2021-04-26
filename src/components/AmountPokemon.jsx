89a08a87212a009ed8e22d26424a51ae3fa61ec6
Adriana <Adrianitargaona@gmail.com>
2021-04-26 15:33:00 +0200

    Building app games store with redux and react Actions, reducer and store add

---
+++ src/components/AmountPokemon.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class AmountPokemon extends Component {
+    render() {
+        return (
+            <React.Fragment>
+            Unidades : 30
+            </React.Fragment>
+        )
+    }
+}
+
+
---
+++ src/components/BuyPokemon.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class BuyPokemon extends Component {
+    render() {
+        return (
+          <div>
+              <button className="btn btn-dark btn-sm mb-2">Comprar Pokemon</button>
+              <button className="btn btn-dark btn-sm mb-1">Regresar Pokemon</button>
+          </div>
+        )
+    }
+}
